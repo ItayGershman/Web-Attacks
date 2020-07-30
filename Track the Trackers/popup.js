78d2f1378970a0f91c2ea61d7e8d3b9d53d40090
itay <48354712+ItayGershman@users.noreply.github.com>
2020-07-30 14:51:57 +0300

    finish extensions logics

--- Track the Trackers/popup.js
+++ Track the Trackers/popup.js
@@ -1,34 +1,54 @@
 const trackers = {
     zoom: 'zoom',
-    google_analytics: 'google-analytics',
-    twitter: 'twitter',
+    google_analytics: 'google_analytics',
+    facebook: 'facebook',
     aliexpress: 'aliexpress',
     linkedIn: 'linkedIn',
 }
 
-window.onload = () => {
-    chrome.runtime.sendMessage({
-        context: "popUpData",
-    }, (res) => {
-        const { current, history } = res
-        getCurrentTrackers(current)
-        getHistoryTrackers(history)
-    });
-}
-const getCurrentTrackers = (current) => {
+const getWebsiteTrackers = (current) => {
     for (const tracker in trackers) {
-        document.getElementById(tracker).innerHTML += ' detected'
-        console.log(tracker)
+        document.getElementById(tracker).innerHTML += ` ${
+      current.trackers[tracker] ? 'Tracks you! 😈' : 'Innocent for now 😇'
+    }`
     }
 }
 
 const getHistoryTrackers = (history) => {
-
+    for (const tracks in history) {
+        const trackerID = document.getElementById(`${tracks}_history`)
+        if (history[tracks].length) {
+            history[tracks].forEach(value => {
+                trackerID.innerHTML += `<li>${value}</li>`
+            })
+        }
+        else {
+            trackerID.innerHTML += `<li><p>No History for this tracker</p></li>`
+        }
+    }
 }
 
-// const currentHandler = (current) => {
-//     for (const tracker in trackers) {
-//         document.getElementById(tracker).innerHTML += ' detected'
-//         console.log(tracker)
-//     }
-// }
+window.onload = () => {
+    chrome.runtime.sendMessage({
+            context: 'getTrackers',
+        },
+        (res) => {
+            const { current, history } = res
+            getWebsiteTrackers(current)
+            getHistoryTrackers(history)
+        }
+    )
+    //Add onClick to buttons
+    const acc = document.getElementsByClassName('accordion')
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener('click', function() {
+            this.classList.toggle('active')
+            const panel = this.nextElementSibling
+            if (panel.style.display === 'block') {
+                panel.style.display = 'none'
+            } else {
+                panel.style.display = 'block'
+            }
+        })
+    }
+}
